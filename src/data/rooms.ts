df051ac1937e39b9ae5f04709bb5f80df7484344
JC <jc@jccm.com.ve>
2026-06-04 20:25:59 -0400

    feat: actualiza descripciones de habitaciones para mayor claridad y atractivo

--- src/data/rooms.ts
+++ src/data/rooms.ts
@@ -12,8 +12,7 @@ export const rooms: Room[] = [
 	{
 		title: "Estándar King",
 		slug: "estandar-king",
-		description:
-			"Tu escapada junto al mar te espera aquí. Habitaciones con espléndida vista al jardín y un toque cercano a la playa, decoradas con inspiración marina contemporánea. Exclusivamente para parejas.",
+		description: "¡Tu escapada junto al mar te espera aquí! Habitaciones con espléndida vista al jardín y un toque cercano a la playa.",
 		image: "https://i.imgur.com/hDdwfD3.png",
 		acreage: "30 m²",
 		guests: "3",
@@ -22,8 +21,7 @@ export const rooms: Room[] = [
 	{
 		title: "Beach Loft",
 		slug: "beach-loft",
-		description:
-			"El Beach Loft es un refugio práctico y acogedor, ideal para quienes buscan disfrutar de la cercanía del mar en un ambiente confortable. Este espacio bien distribuido ofrece una estancia agradable a solo unos pasos de la playa.",
+		description: "El Beach Loft es un refugio práctico y acogedor, ideal para quienes buscan disfrutar de la cercanía del mar en un ambiente confortable.",
 		image: "https://i.imgur.com/KTobwyy.png",
 		acreage: "30 m²",
 		guests: "4",
@@ -32,8 +30,7 @@ export const rooms: Room[] = [
 	{
 		title: "Beach Studio",
 		slug: "beach-studio",
-		description:
-			"Disfruta de la comodidad y la conveniencia en este estudio encantador, ubicado a pocos pasos de Playa Cocles. Ideal para viajeros y profesionales, el estudio cuenta con una amplia cama King en el segundo piso, aire acondicionado para tu confort, y conexión WiFi de alta velocidad. El primer piso ofrece una cocina completamente equipada para preparar tus platos favoritos. Con un escritorio espacioso, este lugar es perfecto para trabajar en un entorno tranquilo y relajante.",
+		description: "Habitación triple económica combina practicidad con todo lo que necesitas para disfrutar de tu estadía en nuestro resort.",
 		image: "https://i.imgur.com/roHz8lv.png",
 		acreage: "30 m²",
 		guests: "3",
@@ -42,8 +39,7 @@ export const rooms: Room[] = [
 	{
 		title: "Beach Apartment",
 		slug: "beach-apartment",
-		description:
-			"Despierta cada mañana en nuestro apartamento vacacional y siente la cercanía del mar Caribe. Ubicado a solo unos pasos de la paradisíaca Playa Cocles, este refugio ofrece la combinación perfecta de lujo y proximidad a la playa.",
+		description: "Despierta cada mañana en nuestro apartamento vacacional y siente la cercanía del mar Caribe. Ubicado a solo unos pasos de la paradisíaca Playa Cocles.",
 		image: "https://i.imgur.com/UDIGFb5.jpeg",
 		acreage: "30 m²",
 		guests: "5",
@@ -52,8 +48,7 @@ export const rooms: Room[] = [
 	{
 		title: "Triple Económica",
 		slug: "triple-economica",
-		description:
-			"¡Tu espacio cómodo y funcional te está esperando! Nuestra habitación triple económica combina practicidad con todo lo que necesitas para disfrutar de tu estadía en nuestro resort. Con una habitación principal con cama matrimonial y una cama individual, esta opción es perfecta para grupos pequeños que buscan una experiencia acogedora.",
+		description: "Nuestras habitaciones estándar son nuestro alojamiento más popular. Tienen vista al jardín.",
 		image: "https://i.imgur.com/mxszw4k.png",
 		acreage: "30 m²",
 		guests: "3",
@@ -62,8 +57,7 @@ export const rooms: Room[] = [
 	{
 		title: "Habitación Superior King",
 		slug: "superior-king",
-		description:
-			"Nuestras exclusivas habitaciones superiores, ubicadas en el sector selvático del resort, representan una opción de alojamiento excepcionalmente adecuada para parejas que buscan una experiencia inmersiva en la naturaleza. Con una impresionante vista a la exuberante selva, cada una de estas habitaciones ofrece una terraza privada equipada con hamacas y un conjunto de sillas de exterior, brindando el entorno perfecto para disfrutar placenteras tardes al son de los sonidos característicos de la selva y su fauna.",
+		description: "Nuestras habitaciones superior se encuentran en el sector selva del resort, son una excelente opción para parejas.",
 		image: "https://i.imgur.com/sLAn159.png",
 		acreage: "35 m²",
 		guests: "3",
@@ -72,8 +66,7 @@ export const rooms: Room[] = [
 	{
 		title: "Bungalow King",
 		slug: "bungalow-king",
-		description:
-			"Situados en la zona selvática del hotel, rodeados de árboles centenarios y exuberantes flores tropicales, nuestros dos bungalows de madera ofrecen una extraordinaria opción para parejas en busca de un refugio tranquilo y natural.",
+		description: "Situados en la zona selvática del hotel, rodeados de árboles centenarios y exuberantes flores tropicales.",
 		image: "https://i.imgur.com/OHZmNxu.png",
 		acreage: "40 m²",
 		guests: "3",
@@ -82,8 +75,7 @@ export const rooms: Room[] = [
 	{
 		title: "Bungalow con 2 camas Queen",
 		slug: "bungalow-queen",
-		description:
-			"Nuestras habitaciones tipo bungalow con dos camas queen, ubicadas en el sector selvático del resort, representan una opción excepcional para parejas en búsqueda de una experiencia tranquila. Con una impresionante vista a la exuberante selva, cada una de estas habitaciones cuenta con una terraza privada equipada con hamacas y un juego de sillas de exterior, creando el entorno perfecto para disfrutar apacibles tardes, escuchando inigualables sonidos de la selva y su variada fauna.",
+		description: "Nuestras bungalow jungle rooms con 2 camas queen se encuentran en el sector selva del resort, son una excelente opción para parejas.",
 		image: "https://i.imgur.com/O2BheRC.png",
 		acreage: "35 m²",
 		guests: "4",
@@ -92,8 +84,7 @@ export const rooms: Room[] = [
 	{
 		title: "Junior Suite",
 		slug: "junior-suite",
-		description:
-			"Ubicada en el corazón de nuestra exuberante jungla y a pasos de la piscina principal, esta exclusiva habitación especial destaca por su generoso espacio de 80 metros cuadrados, convirtiéndola en la opción más amplia disponible en nuestro establecimiento. Equipada con dos camas queen size y un encantador porche provisto de hamacas y sillas, esta habitación ofrece un ambiente de comodidad y relajación incomparables.",
+		description: "Ubicada en el sector de la jungla, cerca de la piscina principal, esta habitación especial es – con sus 80 metros cuadrados. – la más espaciosa que ofrecemos.",
 		image: "https://i.imgur.com/PQuYFD9.png",
 		acreage: "80 m²",
 		guests: "5",
@@ -103,7 +94,7 @@ export const rooms: Room[] = [
 		title: "Jungle House",
 		slug: "jungle-house",
 		description:
-			"Situada dentro de las instalaciones del exclusivo Cariblue Beach and Jungle Resort, esta residencia se presenta como la elección ideal para familias de hasta 7 personas. Consta de dos dormitorios interconectados por un porche que alberga un espacioso salón/comedor y una cocina tipo americana.",
+			"Ubicados en el sector de la selva del hotel, rodeados de arboles centenarios y flores tropicales, nuestros tres bungalows de madera son una excelente opción para familias que quieran descansar rodeados de naturaleza.",
 		image: "https://i.imgur.com/7vvtt44.png",
 		acreage: "90 m²",
 		guests: "7",
@@ -112,8 +103,7 @@ export const rooms: Room[] = [
 	{
 		title: "Ocean Beach View Suites",
 		slug: "ocean-beach-view-suites",
-		description:
-			"Ubicado frente al mar, este exclusivo módulo se compone de tres suites en la planta alta, todas ellas ofreciendo una magnífica vista panorámica de la playa Cocles.",
+		description: "Ubicado frente al mar, este módulo consta de 3 en la planta alta que tienen una magnifica vista a la playa Cocles.",
 		image: "https://i.imgur.com/B3XIXzr.png",
 		acreage: "55 m²",
 		guests: "6",
@@ -122,8 +112,7 @@ export const rooms: Room[] = [
 	{
 		title: "Ocean Pool View Suites",
 		slug: "ocean-pool-view-suites",
-		description:
-			"Situado en una ubicación privilegiada frente al mar, este distinguido módulo consta de tres suites en la planta baja, todas ellas con acceso directo a una exquisita piscina con cascada.",
+		description: "Ubicado frente al mar, este módulo consta de 3 suites en la planta baja que tienen salida directa a una piscina con cascada.",
 		image: "https://i.imgur.com/NIorB4b.png",
 		acreage: "55 m²",
 		guests: "6",
